40508178b3d203ed07465479871bfcdc69eeed1a
Jerald Abille <thejeraldo@gmail.com>
2018-03-17 00:22:17 +0400

    Initial commit.

---
+++ EPGTests/EPGTests.swift
@@ -0,0 +1,36 @@
+//
+//  EPGTests.swift
+//  EPGTests
+//
+//  Created by Jerald Abille on 3/17/18.
+//  Copyright © 2018 Jerald Abille. All rights reserved.
+//
+
+import XCTest
+@testable import EPG
+
+class EPGTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
